c322a47427037ac3fb401f67ff85abf8e6c25fc6
Vinicius Miyata <viyudi@hotmail.com>
2025-02-27 15:43:33 -0300

    Exception handling - findById

--- src/main/java/com/project/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/project/demo/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.project.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.demo.entities.OrderItem;
+import com.project.demo.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK>{
 	
 }
